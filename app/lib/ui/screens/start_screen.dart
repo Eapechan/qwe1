188674983c976a9600a79fd8e821baedfc52e628
Eapechan <haroeapen@gmail.com>
2026-08-06 13:32:25 +0530

    fix: add missing go_router import to start_screen.dart

--- app/lib/ui/screens/start_screen.dart
+++ app/lib/ui/screens/start_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:go_router/go_router.dart';
 import 'package:qwe1/state/servers/server_provider.dart';
 import 'package:qwe1/ui/theme/app_theme.dart';
 import 'package:qwe1/ui/widgets/animated_background.dart';
